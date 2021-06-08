af49f987fa7ab459d064320f35b212ed76cfb7e6
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-08 05:45:52 -0500

    Complete password length function

--- Develop/script.js
+++ Develop/script.js
@@ -3,41 +3,40 @@
 // FUNCTIONS NECESSARY FOR ACCEPTANCE CRITERIA
 
   // functions to prompt user to choose which password criteria to include 
+    
     // function to prompt user for password length
-    var lengthSelect = function() {
-      window.prompt("How many characters long shall your password to be? Please enter a number between 8 and 128, inclusive.")
+      let lengthSelect = function() {
+        debugger;
+        let length = "";
 
-      // if else statement
-      lengthSelect();
-      if (lengthSelect >= 8 && lengthSelect <= 128) {
-        console.log("Yippee!")
-      }
-      else console.log("Invalid response.")
+        while (length === "" || length === null || length < 8 || length > 128 ) {
+          length = prompt("How many characters long shall your password to be? Please enter a number between 8 and 128, inclusive.")
+        }
 
-    };
-
+        console.log("You have chosen a password length of " + length + " characters.");
+        return length;
+      };
 
     // function to prompt user for charset (lowercase, uppercase, numeric, and/or special characters))
-    // question: should the below be window.prompt, or is window.confirm preferable in this case?
 
-    var charSelect = function() {
-      window.confirm("Shall your password include lowercase characters?")
+      let charSelect = function() {
+        window.confirm("Shall your password include lowercase characters?")
 
-      // if else statement
+        // if else statement
 
-      window.confirm("Shall your password include UPPERCASE characters?")
+        window.confirm("Shall your password include UPPERCASE characters?")
 
-      // if else statement
+        // if else statement
 
-      window.confirm("Shall your password include num3r1c values?")
+        window.confirm("Shall your password include num3r1c values?")
 
-      // if else statement
+        // if else statement
 
-      window.confirm("Shall your password include $pec!al characters?")
+        window.confirm("Shall your password include $pec!al characters?")
 
-      // if else statement
+        // if else statement
 
-    };
+      };
 
   // function to validate selection criteria (at least one character type must be accepted)
 
@@ -49,6 +48,13 @@
 
     // window.display("Your password is " + [function output generated above])
 
+
+// PASSWORD DATA TO BE STORED
+let password = {
+  length = lengthSelect(),
+  charSet = charSelect(),
+  // function to select number of characters in charSelect based on password.length
+}
 
 // ****** //
 
