cf88844127774cf61fd069290f274e935481bff7
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-08 16:20:47 -0500

    Add functionality for selection of all characters

--- Develop/script.js
+++ Develop/script.js
@@ -2,72 +2,80 @@
 
 // FUNCTIONS NECESSARY FOR ACCEPTANCE CRITERIA
 
-  // functions to prompt user to choose which password criteria to include 
-    
-    // function to prompt user for password length
-      let lengthSelect = function() {
-        debugger;
-        let length = "";
+// functions to prompt user to choose which password criteria to include 
 
-        while (length === "" || length === null || length < 8 || length > 128 ) {
-          length = window.prompt("How many characters long shall your password to be? Please enter a number between 8 and 128, inclusive.")
-        }
+// function to prompt user for password length
+let lengthSelect = function () {
+  // debugger;
+  let length = "";
 
-        console.log("You have chosen a password length of " + length + " characters.");
-        return length;
-      };
+  while (length === "" || length === null || length < 8 || length > 128) {
+    length = window.prompt("How many characters long shall your password to be? Please enter a number between 8 and 128, inclusive.")
+  }
 
-    // function to prompt user for charset (lowercase, uppercase, numeric, and/or special characters))
+  console.log("You have chosen a password length of " + length + " characters.");
+  return length;
+};
 
-      let charSelect = function() {
+// function to prompt user for charset (lowercase, uppercase, numeric, and/or special characters))
 
-        let charSet = "";
+let charSelect = function () {
 
-        // function to ensure at least one character type must be accepted)
-        while (charSet === "" || charSet === null) {
-          window.alert("You must choose at least one of the following to be featured in your password: lowercase characters, UPPERCASE characters, num3r1c values, $pec!al characters.")
-          lowercase = window.confirm("Shall your password include lowercase letters?")
+  let lowercase, uppercase, numeric, special;
+  let charSet = "";
 
-          // if user chooses to include lowercase letters, log to console and add array of lowercase letters to lowercase variable
-          if(lowercase) {
-            console.log("You have chosen to include lowercase letters in your password.");
-            let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-          }
-          else {
-            console.log("You have chosen to exclude lowercase letters from your password.");
-            lowercase = ""
-          }
-          return lowercase; 
-        }
-      };
+  // function to ensure at least one character type must be accepted)
+  while (!lowercase && !uppercase && !numeric && !special) {
+    window.alert("You must choose at least one of the following to be featured in your password: lowercase characters, UPPERCASE characters, num3r1c values, $pec!al characters.")
+    lowercase = window.confirm("Shall your password include lowercase letters?")
+    uppercase = window.confirm("Shall your password include uppercase letters?")
+    // numeric = window.confirm("Shall your password include numeric characters?")
+    // special = window.confirm("Shall your password include special characters?")
 
-        window.confirm("Shall your password include UPPERCASE characters?")
+    // if user chooses to include lowercase letters, log to console and add array of lowercase letters to lowercase variable
+    if (lowercase) {
+      console.log("You have chosen to include lowercase letters in your password.");
+      charSet += "qwertyuiopasdfghjklzxcvbnm"
+    }
+    if (uppercase) {
+      charSet += "QWERTYUIOPASDFGHJKLZXCVBNM"
+    }
+    // else {
+    //   console.log("You have chosen to exclude lowercase letters from your password.");
+    //   charSet = ""
+    // }
+  }
+  return charSet;
+};
 
-        // if else statement
 
-        window.confirm("Shall your password include num3r1c values?")
+// window.confirm("Shall your password include UPPERCASE characters?")
 
-        // if else statement
+// // if else statement
 
-        window.confirm("Shall your password include $pec!al characters?")
+// window.confirm("Shall your password include num3r1c values?")
 
-        // if else statement
+// // if else statement
 
-  // function to generate a password that meets criteria established based on user input
+// window.confirm("Shall your password include $pec!al characters?")
 
-    // likely a use of Math.floor(Math.random() * [length]
+// if else statement
 
-  // function to display password in an alert or write it on the page
+// function to generate a password that meets criteria established based on user input
 
-    // window.display("Your password is " + [function output generated above])
+// likely a use of Math.floor(Math.random() * [length]
+
+// function to display password in an alert or write it on the page
+
+// window.display("Your password is " + [function output generated above])
 
 
 // PASSWORD DATA TO BE STORED
-let password = {
-  length = lengthSelect(),
-  charSet = charSelect(),
-  // function to select number of characters in charSelect based on password.length
-}
+// let password = {
+//   length: lengthSelect(),
+//   charSet: charSelect(),
+//   // function to select number of characters in charSelect based on password.length
+// }
 
 // ****** //
 
@@ -77,10 +85,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passwordLength = lengthSelect();
+  var charBank = charSelect();
+  let charArr = charBank.split('');
+
+  for (i = 0; i < passwordLength; i++) {
+    let randomIndex = Math.floor(Math.random() * charBank.length)
+    console.log(charArr[randomIndex], randomIndex)
+  }
+
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = charBank;
 
 }
 
