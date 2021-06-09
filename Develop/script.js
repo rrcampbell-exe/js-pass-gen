ad4b0806e54a135bd20536319786fb05fb774ba7
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-09 06:20:27 -0500

    Refactor code for cleanliness

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,5 @@
 // Assignment code here
 
-// FUNCTIONS NECESSARY FOR ACCEPTANCE CRITERIA
-
-// functions to prompt user to choose which password criteria to include 
-
 // function to prompt user for password length
 let lengthSelect = function () {
   // debugger;
@@ -17,7 +13,7 @@ let lengthSelect = function () {
   return length;
 };
 
-// function to prompt user for charset (lowercase, uppercase, numeric, and/or special characters))
+// function to prompt user for charset choices (lowercase, uppercase, numeric, and/or special characters))
 
 let charSelect = function () {
 
@@ -52,53 +48,21 @@ let charSelect = function () {
   return charSet;
 };
 
-
-// window.confirm("Shall your password include UPPERCASE characters?")
-
-// // if else statement
-
-// window.confirm("Shall your password include num3r1c values?")
-
-// // if else statement
-
-// window.confirm("Shall your password include $pec!al characters?")
-
-// if else statement
-
-// function to generate a password that meets criteria established based on user input
-
-// likely a use of Math.floor(Math.random() * [length]
-
-// function to display password in an alert or write it on the page
-
-// window.display("Your password is " + [function output generated above])
-
-
-// PASSWORD DATA TO BE STORED
-// let password = {
-//   length: lengthSelect(),
-//   charSet: charSelect(),
-//   // function to select number of characters in charSelect based on password.length
-// }
-
-// ****** //
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var passwordLength = lengthSelect();
-  var charBank = charSelect();
+  let passwordLength = lengthSelect();
+  let charBank = charSelect();
   let charArr = charBank.split('');
 
   for (i = 0; i < passwordLength; i++) {
     let randomIndex = Math.floor(Math.random() * charBank.length)
-    console.log(charArr[randomIndex], randomIndex)
+    console.log(charArr[randomIndex])
   }
 
-  var passwordText = document.querySelector("#password");
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = charBank;
 
